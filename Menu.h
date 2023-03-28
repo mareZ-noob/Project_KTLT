10046700b712397b9efc96f336312636870a138a
ĐINH DUY TRIẾT <119549833+TRIETDINHDUY@users.noreply.github.com>
2023-03-28 18:01:46 +0700

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -11,4 +11,14 @@ void menu(int choice);
 void quit();
 void login(string &email);
 
+//DINH DUY TRIET
+void selectionMenu(int selection, int x, int y, int w, int h, int textColor, int buttonColor, int backgroundColor, string text);
+void selectionMenuLogin(int selection, int x, int y, int w, int h, int textColor, int buttonColor, int backgroundColor, string text);
+void printMenu(int selection, int x, int y, int w, int h, int textColor, int buttonColor, int backgroundColor, string text);
+void printMenuLogin(int selection, int x, int y, int w, int h, int textColor, int buttonColor, int backgroundColor, string text);
+int MainMenuBack();
+void MenuLogin();
+void MainMenu();
+void login1(string& email);
+
 #endif // __MENU_H__
