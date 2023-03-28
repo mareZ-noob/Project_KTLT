8ff57ed354dee57d7e0b809c02268bf422ddab5a
ĐINH DUY TRIẾT <119549833+TRIETDINHDUY@users.noreply.github.com>
2023-03-28 17:56:26 +0700

    Update Graphic.cpp

--- Graphic.cpp
+++ Graphic.cpp
@@ -13,3 +13,19 @@ void art(string fileName, int pX, int pY)
     }
     file.close();
 }
+
+//Draw art at position (x, y) (Do mấy hôm trước chưa update hàm art nên t làm vậy)
+void art_at_pos(string fileName, int textColor, int backgroundColor, int x, int y)
+{
+	ifstream file;
+	file.open(fileName);
+	string line;
+	while (getline(file, line))
+	{
+		moveCursor(x, y++);
+		BackgroundColor(backgroundColor, textColor);
+		TextColor(textColor);
+		cout << line << endl;
+	}
+	file.close();
+}
