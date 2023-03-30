0f205891f59ad7bcf11b6cab0d112387e7ca45fd
ĐINH DUY TRIẾT <119549833+TRIETDINHDUY@users.noreply.github.com>
2023-03-30 14:07:40 +0700

    Update Graphic.cpp

--- Graphic.cpp
+++ Graphic.cpp
@@ -52,9 +52,16 @@ void pokemon_ball(string fileName, int backgroundColor, int x, int y) {
         int posX = x, posY = y;
         while (file.get(c)) {
             if (c == '7') {
-                consoleColor(backgroundColor, 4);
+                consoleColor(backgroundColor, LIGHT_RED);
                 moveCursor(posX, posY);
-                TextColor(4);
+                TextColor(LIGHT_RED);
+                cout << c;
+                posX++;
+            }
+            else if (c == '0') {
+                consoleColor(backgroundColor, LIGHT_WHITE);
+                moveCursor(posX, posY);
+                TextColor(LIGHT_WHITE);
                 cout << c;
                 posX++;
             }
@@ -63,9 +70,60 @@ void pokemon_ball(string fileName, int backgroundColor, int x, int y) {
                 posX = x;
             }
             else {
-                consoleColor(backgroundColor, 15);
+                consoleColor(backgroundColor, GRAY);
                 moveCursor(posX, posY);
-                TextColor(15);
+                TextColor(GRAY);
+                cout << c;
+                posX++;
+            }
+        }
+        file.close();
+    }
+}
+
+void pikachu_small(string fileName, int backgroundColor, int x, int y) {
+    ifstream file(fileName);
+
+    if (file.is_open()) {
+        char c;
+        int posX = x, posY = y;
+        while (file.get(c)) {
+            if (c == 'B') {
+                consoleColor(backgroundColor, LIGHT_YELLOW);
+                moveCursor(posX, posY);
+                TextColor(LIGHT_YELLOW);
+                cout << c;
+                posX++;
+            }
+            else if (c == '5') {
+                consoleColor(backgroundColor, BLACK);
+                moveCursor(posX, posY);
+                TextColor(BLACK);
+                cout << c;
+                posX++;
+            }
+            else if (c == '8') {
+                consoleColor(backgroundColor, GRAY);
+                moveCursor(posX, posY);
+                TextColor(GRAY);
+                cout << c;
+                posX++;
+            }
+            else if (c == '7') {
+                consoleColor(backgroundColor, LIGHT_RED);
+                moveCursor(posX, posY);
+                TextColor(LIGHT_RED);
+                cout << c;
+                posX++;
+            }
+            else if (c == '\n') {
+                posY++;
+                posX = x;
+            }
+            else {
+                consoleColor(backgroundColor, LIGHT_WHITE);
+                moveCursor(posX, posY);
+                TextColor(LIGHT_WHITE);
                 cout << c;
                 posX++;
             }
